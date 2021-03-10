ac71da6d486144b0b8bd9e650036771a75d51818
NEO <neo.one.mail@gmail.com>
2021-03-10 02:51:20 +0200

    add user input

--- main.py
+++ main.py
@@ -7,48 +7,64 @@ from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-C_KEY = os.getenv('C_KEY')
-C_SECRET = os.getenv('C_SECRET')
-A_TOKEN = os.getenv('A_TOKEN')
-A_SECRET = os.getenv('A_SECRET')
+class TextBlobSen:
+    def __init__(self, query):
+        self.query = query
 
-auth_handler = tweepy.OAuthHandler(consumer_key=C_KEY, consumer_secret=C_SECRET)
-auth_handler.set_access_token(A_TOKEN, A_SECRET)
+    def stream(self):
+        C_KEY = os.getenv('C_KEY')
+        C_SECRET = os.getenv('C_SECRET')
+        A_TOKEN = os.getenv('A_TOKEN')
+        A_SECRET = os.getenv('A_SECRET')
 
-api = tweepy.API(auth_handler)
+        auth_handler = tweepy.OAuthHandler(consumer_key=C_KEY, consumer_secret=C_SECRET)
+        auth_handler.set_access_token(A_TOKEN, A_SECRET)
 
-polarity = 0
-positive = 0
-negative = 0
-neutral = 0
-
-query = 'star wars'
-tweet_num = 200
-
-tweets = tweepy.Cursor(api.search, q=query, lang='en').items(tweet_num)
-
-for tweet in tweets:
-    trimmed_tweet = tweet.text.replace('RT', '')
-
-    if trimmed_tweet.startswith(' @'): 
-        index = trimmed_tweet.index(':')
-        trimmed_tweet = trimmed_tweet[index + 2:]
+        api = tweepy.API(auth_handler)
     
-    if trimmed_tweet.startswith('@'): 
-        index = trimmed_tweet.index(' ')
-        trimmed_tweet = trimmed_tweet[index + 2:]
+        tweet_num = 200
+        tweets = tweepy.Cursor(api.search, q=self.query, lang='en').items(tweet_num)
+
+        return tweets
+
+
+    def trim(self, char_to_trim, tweet):
+        index = tweet.index(char_to_trim)
+        final_tweet = tweet[index + 2:]
+
+    def get_sentiment(self, tweets):
+        polarity = 0
+        positive = 0
+        negative = 0
+        neutral = 0
     
-    sentiment = TextBlob(trimmed_tweet)
+        for tweet in tweets:
+            trimmed_tweet = tweet.text.replace('RT', '')
 
-    if sentiment.polarity > 0: positive += 1
-    elif sentiment.polarity < 0: negative += 1
-    elif sentiment.polarity == 0: neutral += 1
+            if trimmed_tweet.startswith(' @'): self.trim(':', trimmed_tweet)
+            if trimmed_tweet.startswith('@'): self.trim(' ', trimmed_tweet)
+            
+            sentiment = TextBlob(trimmed_tweet)
 
-    polarity += sentiment.polarity
+            if sentiment.polarity > 0: positive += 1
+            elif sentiment.polarity < 0: negative += 1
+            elif sentiment.polarity == 0: neutral += 1
 
-print(f'Analysis for {query}')
+            polarity += sentiment.polarity
 
-print(f'positive tweets => {positive}')
-print(f'neutral tweets => {neutral}')
-print(f'negative tweets => {negative}')
-print(f'polarity => {polarity}')
+        return polarity, positive, negative, neutral
+
+    def analyise(self):
+        streamed_tweets = self.stream()
+        polarity, positive, negative, neutral = self.get_sentiment(streamed_tweets)
+        
+        print(f'Analysis for {self.query}')
+        print(f'positive tweets => {positive}')
+        print(f'neutral tweets => {neutral}')
+        print(f'negative tweets => {negative}')
+        print(f'polarity => {polarity}')
+
+query = input('What topic would you like to analyise: ')
+print('Loading ...')
+text_blob_sen = TextBlobSen(query)
+text_blob_sen.analyise()
